45829a9b8d55b989b0d394678d17cf06fca9f12c
Forrest Tanaka <fytanaka@stanford.edu>
2018-06-11 08:00:18 -0700

    Temporary commit while testing demos

--- src/encoded/static/components/cart/toggle.js
+++ src/encoded/static/components/cart/toggle.js
@@ -2,24 +2,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { svgIcon } from '../../libs/svg-icons';
 import { addToCart, removeFromCart } from './actions';
 
 
 // Button to add the current object to the cart, or to remove it.
 const CartToggleComponent = ({ cart, savedItems, current, onAddToCartClick, onRemoveFromCartClick }) => {
-    const checked = cart.indexOf(current) > -1;
+    const inCart = cart.indexOf(current) > -1;
     const saved = savedItems.indexOf(current) > -1;
-    const onChange = checked ? onRemoveFromCartClick : onAddToCartClick;
+    const onClick = inCart ? onRemoveFromCartClick : onAddToCartClick;
 
     return (
         <div className="cart__toggle">
             <div className="cart__checkbox">
-                <input id={current} type="checkbox" checked={checked} onChange={onChange} title={`${checked ? 'Remove item from' : 'Add item to'} cart`} />
+                <button onChange={onClick}>{svgIcon('cart')}</button>
                 <label htmlFor={current}>
-                    <span className="sr-only">{checked ? `Remove ${current} from cart` : `Add ${current} to cart`}</span>
+                    <span className="sr-only">{inCart ? `Remove ${current} from cart` : `Add ${current} to cart`}</span>
                 </label>
             </div>
-            {checked !== saved ?
+            {inCart !== saved ?
                 <div className="cart__saved-status">
                     <i className="icon icon-circle cart__saved-status-icon" title="Unsaved cart item" />
                 </div>
