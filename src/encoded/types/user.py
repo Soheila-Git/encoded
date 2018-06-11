acfc5ba314fb6bece4d406836d92a205db4b04a7
Forrest Tanaka <fytanaka@stanford.edu>
2018-06-11 12:29:25 -0700

    Return user reverse links to carts

--- src/encoded/types/user.py
+++ src/encoded/types/user.py
@@ -10,6 +10,7 @@ from pyramid.security import (
 )
 from .base import (
     Item,
+    paths_filtered_by_status,
 )
 from snovault import (
     CONNECTION,
@@ -54,6 +55,9 @@ class User(Item):
     embedded = [
         'lab',
     ]
+    rev = {
+        'carts': ('Cart', 'submitted_by'),
+    }
     STATUS_ACL = {
         'current': [(Allow, 'role.owner', ['edit', 'view_details'])] + USER_ALLOW_CURRENT,
         'deleted': USER_DELETED,
@@ -86,6 +90,18 @@ class User(Item):
         objects = (request.embed('/', str(uuid), '@@object') for uuid in uuids)
         return [obj for obj in objects if obj['status'] not in ('deleted', 'replaced')]
 
+    @calculated_property(schema={
+        "title": "User's carts",
+        "type": "array",
+        "items": {
+            "type": ['string', 'object'],
+            "linkFrom": "Cart.submitted_by",
+        },
+    })
+    def carts(self, request, carts):
+        return paths_filtered_by_status(request, carts)
+
+
 
 @view_config(context=User, permission='view', request_method='GET', name='page')
 def user_page_view(context, request):
@@ -106,7 +122,7 @@ def user_page_view(context, request):
 def user_basic_view(context, request):
     properties = item_view_object(context, request)
     filtered = {}
-    for key in ['@id', '@type', 'uuid', 'lab', 'title']:
+    for key in ['@id', '@type', 'uuid', 'lab', 'title', 'carts']:
         try:
             filtered[key] = properties[key]
         except KeyError:
